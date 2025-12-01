caa7d80985762d03db63cb377e9a9ba032578740
Zash60 <xath06@gmail.com>
2025-11-30 21:51:46 -0300

    Update CaptureVideoDialog.kt

--- test/src/main/java/com/suzukiplan/emulator/nes/test/CaptureVideoDialog.kt
+++ test/src/main/java/com/suzukiplan/emulator/nes/test/CaptureVideoDialog.kt
@@ -2,25 +2,29 @@ package com.suzukiplan.emulator.nes.test
 
 import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.FragmentManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.fragment.app.DialogFragment
 
 class CaptureVideoDialog : DialogFragment() {
-    private lateinit var capture: Bitmap
 
-    fun show(manager: FragmentManager, capture: Bitmap) {
-        this.capture = capture
+    private lateinit var bitmap: Bitmap
+
+    fun show(manager: androidx.fragment.app.FragmentManager, bitmap: Bitmap) {
+        this.bitmap = bitmap
         isCancelable = true
-        show(manager, tag)
+        super.show(manager, "CaptureVideoDialog")
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater?.inflate(R.layout.dialog_capture_video, container)
-        view?.findViewById<ImageView>(R.id.capture_preview)?.setImageBitmap(capture)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.dialog_capture_video, container, false)
+        view.findViewById<ImageView>(R.id.capture_preview).setImageBitmap(bitmap)
         return view
     }
 }
